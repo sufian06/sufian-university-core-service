1ca51fa0fe3283bae4bd100c39c4d45723e18c02
Abu sufian <75737338+sufian06@users.noreply.github.com>
2023-12-25 14:23:56 +0600

    logger name update and a folder remove from log/winston folder

--- src/shared/logger.ts
+++ src/shared/logger.ts
@@ -25,7 +25,7 @@ const logger = createLogger({
         'logs',
         'winston',
         'successes',
-        'phu-%DATE%-success.log'
+        'sum-%DATE%-success.log'
       ),
       datePattern: 'YYYY-DD-MM-HH',
       zippedArchive: true,
@@ -46,7 +46,7 @@ const errorlogger = createLogger({
         'logs',
         'winston',
         'errors',
-        'phu-%DATE%-error.log'
+        'sum-%DATE%-error.log'
       ),
       datePattern: 'YYYY-DD-MM-HH',
       zippedArchive: true,
